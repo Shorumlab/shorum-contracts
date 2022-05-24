8e984faf504cdd18283fd6b25cb66279e0135bc5
jamie <starit1992@gmail.com>
2022-05-24 23:54:37 +0800

    add task: set-follow-module

--- tasks/set-follow-module.ts
+++ tasks/set-follow-module.ts
@@ -12,7 +12,7 @@ task('set-follow-module', 'set a follow module for a profile').setAction(async (
   const currencyAddr = '0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1';
   const profileId = 1;
   const amountToBePaid = 1e17;
-  const moduleAddr = addrs('backer follow module');
+  const moduleAddr = addrs['backer follow module'];
   const dataStructure = ['uint256', 'address', 'address'];
 
   let governanceNonce = await hre.ethers.provider.getTransactionCount(governance.address);
